f2e5d5ed3199301a0a2c35bb85fbb19cd3becf1b
youvilir <65822918+youvilir@users.noreply.github.com>
2021-10-11 12:45:38 +0300

    Add files via upload

---
+++ url/test.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Окт 10 2021 г., 23:17
+-- Версия сервера: 8.0.19
+-- Версия PHP: 7.4.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `short`
+--
+
+CREATE TABLE `short` (
+  `id` int NOT NULL,
+  `url` text NOT NULL,
+  `short_key` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `short`
+--
+
+INSERT INTO `short` (`id`, `url`, `short_key`) VALUES
+(43, '\"https://ok.ru\"', '\"edpo9\"'),
+(44, '\"https://vk.com/mmmiiaaaa\"', '\"rk1nm\"');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `short`
+--
+ALTER TABLE `short`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `short`
+--
+ALTER TABLE `short`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
